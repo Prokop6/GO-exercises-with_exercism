6f5b1b2d17ff8faf2552110fa6262c284398299c
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-08 00:02:26 +0100

    done

--- exercism/go/vehicle-purchase/vehicle_purchase.go
+++ exercism/go/vehicle-purchase/vehicle_purchase.go
@@ -1,16 +1,41 @@
 package purchase
 
+import (
+	"fmt"
+	"sort"
+)
+
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	var resp bool 
+	switch kind {
+	case "car", "truck":
+		resp = true
+		default: 
+		resp = false 
+	}
+	return resp
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	optionsSlc := []string{option1, option2}
+	sort.Strings(optionsSlc)
+	return fmt.Sprintf("%s is clearly the better choice.", optionsSlc[0])
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	var valueAfterTime float64
+	switch {
+	case age >= 10:
+		valueAfterTime = 0.5
+	case age > 3:
+		valueAfterTime = 0.7
+	case age < 3:
+		valueAfterTime = 0.8
+	default:
+			valueAfterTime = 1
+	}
+	return originalPrice * valueAfterTime
 }
