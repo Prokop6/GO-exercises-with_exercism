559a65d7ef7d66a838ca197158cab16e33474275
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-13 21:10:08 +0000

    implemented PreparationTime

--- exercism/go/lasagna-master/lasagna_master.go
+++ exercism/go/lasagna-master/lasagna_master.go
@@ -1,6 +1,14 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, avg int) int {
+	//Accepts a slice of layers and average preparation time per layer [min] to return estimate for total preparation time [min]
+
+	if avg == 0 {
+		avg = 2
+	}
+
+	return avg * len(layers)	
+}
 
 // TODO: define the 'Quantities()' function
 
