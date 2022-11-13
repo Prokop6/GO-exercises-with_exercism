cac96f9ba5e3509cf3320cb0dcdecbe014e240d2
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-13 23:08:34 +0000

    implemented interest

--- exercism/go/interest-is-interesting/interest_is_interesting.go
+++ exercism/go/interest-is-interesting/interest_is_interesting.go
@@ -25,7 +25,13 @@ func InterestRate(balance float64) float32 {
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+
+interestRate := float64(InterestRate(balance))/100 //[%]
+
+interest := balance * interestRate
+
+return interest
+
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
