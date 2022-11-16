2a769554162cfc13729dc06ca5fc41301eb96af8
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-16 16:29:41 +0000

    added CountInFile and ContInRank

--- exercism/go/chessboard/chessboard.go
+++ exercism/go/chessboard/chessboard.go
@@ -2,18 +2,40 @@ package chessboard
 
 // Declare a type named File which stores if a square is occupied by a piece - this will be a slice of bools
 
+type File []bool
+
 // Declare a type named Chessboard which contains a map of eight Files, accessed with keys from "A" to "H"
+
+type Chessboard map[string]File
 
 // CountInFile returns how many squares are occupied in the chessboard,
 // within the given file.
 func CountInFile(cb Chessboard, file string) int {
-	panic("Please implement CountInFile()")
+	for fileDSG, fileValue  := range cb {	
+		if file == fileDSG {
+			var counter int
+
+			for _, inFile := range fileValue {
+				if inFile {
+					counter += 1
+				}
+			} 
+			return counter
+		}
+	} 
+	return 0
 }
 
 // CountInRank returns how many squares are occupied in the chessboard,
 // within the given rank.
 func CountInRank(cb Chessboard, rank int) int {
-	panic("Please implement CountInRank()")
+	var counter int 
+	for _, fileVal := range cb {
+		if fileVal[rank-1] {
+			counter += 1
+		}
+	}
+	return counter
 }
 
 // CountAll should count how many squares are present in the chessboard.
