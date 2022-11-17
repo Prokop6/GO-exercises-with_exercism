151f9a0930ad8325e9a8c695f3824ad1f6da9cd8
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-17 17:36:47 +0000

    corrected errors

--- exercism/go/elons-toys/elons_toys.go
+++ exercism/go/elons-toys/elons_toys.go
@@ -14,8 +14,8 @@ type Car struct {
 //Drive checks if remaining battery is allows do perform a movement, if yes it reduces the Cars battery by battery drain and increases the distance by speed
 func (car *Car) Drive() {
 	if car.battery < car.batteryDrain { 
-		panic("Battery to low!")
-	} 
+		return 
+		} 
 
 	car.distance += car.speed
 	car.battery -= car.batteryDrain
@@ -29,15 +29,16 @@ func (car Car) DisplayDistance() string {
 
 // DisplayBattery returns remaining battery percentage 
 func (car Car) DisplayBattery() string {
-	return fmt.Sprintf("Battery at %d$", car.battery)
+	return fmt.Sprintf("Battery at %d%s", car.battery, "%")
 }
 
 // CanFinish checks if Car is able to finish a track with given length
 func (car Car) CanFinish(distance int) bool {
-	timeToFinish := distance / car.speed
-	
-	powerToFinish := car.batteryDrain * timeToFinish
 
-	return powerToFinish < car.battery
+	timeToFinish := float32(distance) / float32(car.speed)
+	
+	powerToFinish := float32(car.batteryDrain) * timeToFinish
+
+	return powerToFinish <= float32(car.battery)
 
 }
