3893f831e5a1467ab4537c00db950bc4398f889c
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-17 17:15:08 +0000

    implemented Drive

--- exercism/go/elons-toys/elons_toys.go
+++ exercism/go/elons-toys/elons_toys.go
@@ -1,6 +1,23 @@
 package elon
 
-// TODO: define the 'Drive()' method
+/*
+type Car struct {
+	speed int
+	batteryDrain int
+	battery int
+	distance int
+}
+*/
+
+//Drive checks if remaining battery is allows do perform a movement, if yes it reduces the Cars battery by battery drain and increases the distance by speed 
+func (car *Car) Drive() {
+	if car.battery < car.batteryDrain { 
+		panic("Battery to low!")
+	} 
+
+	car.distance += car.speed
+	car.battery -= car.batteryDrain
+
 
 // TODO: define the 'DisplayDistance() string' method
 
