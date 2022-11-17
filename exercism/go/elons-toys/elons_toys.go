a4df2b4cf19f747a68c43433b9d786f2c88846c5
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-17 17:27:05 +0000

    implemented DisplayDistance, DisplayBattery and CanFinis

--- exercism/go/elons-toys/elons_toys.go
+++ exercism/go/elons-toys/elons_toys.go
@@ -1,5 +1,7 @@
 package elon
 
+import "fmt"
+
 /*
 type Car struct {
 	speed int
@@ -9,7 +11,7 @@ type Car struct {
 }
 */
 
-//Drive checks if remaining battery is allows do perform a movement, if yes it reduces the Cars battery by battery drain and increases the distance by speed 
+//Drive checks if remaining battery is allows do perform a movement, if yes it reduces the Cars battery by battery drain and increases the distance by speed
 func (car *Car) Drive() {
 	if car.battery < car.batteryDrain { 
 		panic("Battery to low!")
@@ -18,9 +20,24 @@ func (car *Car) Drive() {
 	car.distance += car.speed
 	car.battery -= car.batteryDrain
 
+}
 
-// TODO: define the 'DisplayDistance() string' method
+// DisplayDistance returns distance of the Car
+func (car Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", car.distance)
+}
 
-// TODO: define the 'DisplayBattery() string' method
+// DisplayBattery returns remaining battery percentage 
+func (car Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d$", car.battery)
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+// CanFinish checks if Car is able to finish a track with given length
+func (car Car) CanFinish(distance int) bool {
+	timeToFinish := distance / car.speed
+	
+	powerToFinish := car.batteryDrain * timeToFinish
+
+	return powerToFinish < car.battery
+
+}
