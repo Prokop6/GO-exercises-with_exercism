53df0a94bd6af04653f3247f479bb2797184a393
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-20 01:22:29 +0000

    solved

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -1,5 +1,23 @@
+// raindrops converts a number into string of raindrop sounds
 package raindrops
 
+import "fmt"
+
+// Convert takes a number and returns a string that contains raindrop sounds corresponding to certain potential factors
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var resp string 
+	if number % 3 == 0 {
+		resp += "Pling"
+	} 
+	if number % 5 == 0 {
+		resp += "Plang"
+	}
+	if number % 7 == 0 {
+		resp += "Plong"
+	}
+	if resp == "" {
+		resp = fmt.Sprint(number)
+		}	
+
+	return resp
 }
