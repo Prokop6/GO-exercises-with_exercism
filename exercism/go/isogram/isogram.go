e8426b51657a267421495d240ad2e5546c01a299
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-11-20 21:10:19 +0100

    implemented IsIsogram

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,5 +1,24 @@
 package isogram
 
+import (
+	"strings"
+)
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+
+	letters := []rune(strings.ToLower(word))
+
+	for i, letter := range letters {
+		if letter == ' '  || letter == '-' {
+			continue
+		}
+		for _, letter2 := range letters[i+1:] {
+			if letter ==  letter2 {
+				
+				return false 
+			}
+		}
+	}
+
+	return true
 }
