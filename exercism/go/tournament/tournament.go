bfa9cee1b1692ac22d3f76a63e9fa1bbae7fa434
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-12-04 01:35:31 +0100

    initial unsuccessfull attempts

--- exercism/go/tournament/tournament.go
+++ exercism/go/tournament/tournament.go
@@ -1,7 +1,17 @@
 package tournament
 
-import "io"
+import (
+	"errors"
+	"fmt"
+	"io"
+)
 
 func Tally(reader io.Reader, writer io.Writer) error {
-	panic("Please implement the Tally function")
+	inputData, err := reader.Read()
+
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+
+	return errors.New("not implemented yet")
 }
