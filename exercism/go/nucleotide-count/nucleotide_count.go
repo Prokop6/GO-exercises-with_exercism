9f05e4527a63f0e0aa4444d87c6b4c98f770292c
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-12-05 23:16:41 +0100

    solved Nucleotide count (#38)
     implemented Histogram, Data, Counts

---
+++ exercism/go/nucleotide-count/nucleotide_count.go
@@ -0,0 +1,36 @@
+package dna
+
+import "errors"
+
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[rune]int  
+
+// DNA is a list of nucleotides. Choose a suitable data type.
+type DNA []rune
+
+// Counts generates a histogram of valid nucleotides in the given DNA.
+// Returns an error if d contains an invalid nucleotide.
+func (d DNA) Counts() (Histogram, error) {
+	h := Histogram{
+		'A': 0,
+		'T': 0,
+		'C': 0,
+		'G': 0,
+	}
+	var emptyHist Histogram
+	for _, nucleotide := range d {
+		switch nucleotide {
+		case 'A':
+			h['A'] += 1
+		case 'T':
+			h['T'] += 1
+		case 'C': 
+			h['C'] += 1
+		case 'G':
+			h['G'] += 1
+		default: 
+		return emptyHist, errors.New("invalid symbol in DNA sample")
+		}
+	}
+	return h, nil
+}
