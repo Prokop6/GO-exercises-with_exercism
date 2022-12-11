ec0c72f537a1afd4f2be64106a8daeb55c31bf6d
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-12-12 00:21:00 +0100

    implemented ToRNA

--- exercism/go/rna-transcription/rna_transcription.go
+++ exercism/go/rna-transcription/rna_transcription.go
@@ -1,5 +1,18 @@
 package strand
 
+var dnaToRna = map[rune]string{
+	'A': "U",
+	'G': "C",
+	'C': "G",
+	'T': "A",
+}
+
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	rna := ""
+
+	for _, n := range dna {
+		rna = rna + dnaToRna[n]
+	}
+
+	return rna
 }
