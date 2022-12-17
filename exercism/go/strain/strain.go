00779f321625f854cad017ca5881c1ec29450989
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-12-18 00:19:38 +0100

    implemented solution

--- exercism/go/strain/strain.go
+++ exercism/go/strain/strain.go
@@ -5,17 +5,48 @@ type Lists [][]int
 type Strings []string
 
 func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
+	var r Ints
+	for _, e := range i {
+		if filter(e) {
+			r = append(r, e)
+		}
+	}
+
+	return r
 }
 
 func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
+	var r Ints
+	for _, e := range i {
+		if (!filter(e)){
+			r = append(r, e)
+		}
+	}
+
+	return r
 }
 
 func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
+	var r Lists
+
+	loopOverSlices: for _, s := range l {
+		if len(l[0]) != len(s) {
+			continue loopOverSlices
+		}
+		if filter(s) {
+		r = append(r, s)
+	}
+}
+	return r
 }
 
 func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
+	var r Strings
+	for _, e := range s {
+		if(filter(e)) {
+			r = append(r, e)
+		}
+	}
+
+	return r
 }
