97a929d95f8efe158131cfe3049f5f4047062a65
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-12-18 00:39:58 +0100

    implemented solution

--- exercism/go/etl/etl.go
+++ exercism/go/etl/etl.go
@@ -1,5 +1,15 @@
 package etl
 
+import "strings"
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	r := make(map[string]int)
+
+	for s, l := range in {
+		for _, e := range l {
+		r[strings.ToLower(e)] = s
+		}
+	}
+
+	return r
 }
