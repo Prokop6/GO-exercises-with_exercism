76850330136ac066540c163d91776d1d00ad117f
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-12-18 22:47:40 +0100

    implemented solution

--- exercism/go/pangram/pangram.go
+++ exercism/go/pangram/pangram.go
@@ -1,5 +1,24 @@
 package pangram
 
+import (
+	"strings"
+)
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+		letters := make(map[string]int)
+		
+		if len(input) == 0 {
+			return false
+		}
+
+		spell: for _, l := range strings.Split(strings.ToLower(input), "") {
+			for _, c := range strings.Split("abcdefghijklmnopqrstuvwxyz", "") {
+				if l == c {
+					letters[l] += 1
+					continue spell
+				}
+			}
+		}
+		
+		return len(letters) == 26
 }
