4bb8723bb833d0b0b833f1b6a7c69f34397baa18
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-12-19 01:41:16 +0100

    implemented solution

--- exercism/go/isbn-verifier/isbn_verifier.go
+++ exercism/go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,60 @@
 package isbn
 
+import (
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func isolateValid(d string) []int {
+	mask := `[0-9Xx]{1}`
+	reg := regexp.MustCompile(mask)
+	
+	digits := reg.FindAllString(d, -1)
+	var resp []int
+
+	for i, d := range digits {
+		if strings.EqualFold(d, "x") {
+			if i != 9 {
+				return nil
+			} else {
+				resp = append(resp, 10)
+			}
+		} else {
+			di, err := strconv.Atoi(d)
+			if err == nil {
+				resp = append(resp, di)
+			}
+		}
+	}
+	return resp
+}
+
+func isolateInvalid(inp string) []string {
+	mask := `[a-wyzA-WYZ]`
+	reg := regexp.MustCompile(mask)
+
+	return reg.FindAllString(inp, -1)
+}
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	if isolateInvalid(isbn) != nil {
+		return false 
+	}
+
+	digits := isolateValid(isbn)
+
+	if len(digits) != 10 {
+		return false
+	}
+
+	sum := 0
+	mlt := 10
+
+	for i, d := range digits {
+		sum += (mlt - i) * d
+	} 
+
+	return math.Mod(float64(sum), 11) == 0
 }
